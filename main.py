2a80b501c2247b095eaa2ac7bcd74b43332474fb
Kyuu <69904775+KyuuLee@users.noreply.github.com>
2022-10-01 01:56:35 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
+# num1 = [ 10, 5, 20, 0, 40, 45, 50]
+# num2 = [40, 5, 10]
 
 num1 = list(map(int, input().split()))
 num2 = list(map(int, input().split()))
 
-# ******************************
-# Make your Code
-# ******************************
-
-# print ('True') or print ('False')
+flag = 0
+for i in range(len(num2)):
+	if ( num2[i] in num1 ):
+		continue
+	else:
+		print ("False")
+		break
+else:
+	print ("True")
